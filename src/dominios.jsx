369f0cba3d729f936206f77472dbee242d98578f
ronnin47 <tempesttempest66@gmail.com>
2024-09-12 12:28:36 -0300

    mitama dorada

--- src/dominios.jsx
+++ src/dominios.jsx
@@ -21,7 +21,7 @@ export const Dominios=({checkDominio,setCheckDominio})=> {
         <button className={`btnNatura mArmas ${checkDominio === 'mArmas' ? 'selected animate__animated animate__flip' : ''}`} value="mArmas" onClick={selecionado}>Maestro de armas</button>
         <button className={`btnNatura mSombras ${checkDominio === 'mSombras' ? 'selected animate__animated animate__flip' : ''}`} value="mSombras" onClick={selecionado}>Maestro de sombras</button>
         <button className={`btnNatura mEspiritual ${checkDominio === 'mEspiritual' ? 'selected animate__animated animate__flip' : ''}`} value="mEspiritual" onClick={selecionado}>Maestro espiritual</button>
-        <button className={`btnNatura hechicero ${checkDominio === 'hechicero' ? 'selected animate__animated animate__flip' : ''}`} value="hechicero" onClick={selecionado}>Hehcicero</button>
+        <button className={`btnNatura hechicero ${checkDominio === 'hechicero' ? 'selected animate__animated animate__flip' : ''}`} value="hechicero" onClick={selecionado}>Hechicero</button>
         <button className={`btnNatura cBakemono ${checkDominio === 'cBakemono' ? 'selected animate__animated animate__flip' : ''}`} value="cBakemono" onClick={selecionado}>Criador Bakemono</button>
         </>
   )
